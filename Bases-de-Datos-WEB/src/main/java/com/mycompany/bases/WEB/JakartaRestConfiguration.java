1a2f81dd1b38790cd7e3a67b26a87ef0055fb7a8
Alan0Y <103816205+Alan0Y@users.noreply.github.com>
2024-06-09 23:29:00 -0600

    Add files via upload

---
+++ Bases-de-Datos-WEB/src/main/java/com/mycompany/bases/WEB/JakartaRestConfiguration.java
@@ -0,0 +1,12 @@
+package com.mycompany.bases.WEB;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Alan DoubleEight4
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
